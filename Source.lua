758c305977d6b1e9cb8c194f6ad92a5e8e0adad3
thanhdat4461 <125394392+thanhdat4461@users.noreply.github.com>
2023-03-18 23:58:36 +0700

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -12,8 +12,8 @@ ContentProvider:PreloadAsync({"rbxassetid://3570695787", "rbxassetid://270889159
 
 local Library = {
     Theme = {
-        MainColor = Color3.fromRGB(38, 45, 71),
-        BackgroundColor = Color3.fromRGB(26, 32, 58),
+        MainColor = Color3.fromRGB(255, 0, 0),
+        BackgroundColor = Color3.fromRGB(0, 0, 0),
         UIToggleKey = Enum.KeyCode.RightControl,
         TextFont = Enum.Font.SourceSansBold,
         EasingStyle = Enum.EasingStyle.Quart
